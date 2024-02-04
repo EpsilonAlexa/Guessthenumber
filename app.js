9d8445ccd2c119c7dfb0b1f87f6f84cd7b822baa
Alexa <79060564+EpsilonAlexa@users.noreply.github.com>
2024-02-04 22:04:51 +0200

    Update app.js

--- app.js
+++ app.js
@@ -3,10 +3,10 @@ while (!maximum) {
     maximum = parseInt(prompt("Enter a valid number!"));
 }
 
+const targetNum = Math.floor(Math.random() * 100);
+
 let guess = prompt("Enter your first guess, less than 100!" (Type 'X' to quit)");
 let attempts = 1;
-
-const targetNum = Math.floor(Math.random() * 100);
 
 while (parseInt(guess) !== targetNum) {
     if (guess === 'X') break;
